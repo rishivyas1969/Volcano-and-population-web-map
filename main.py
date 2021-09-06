0a208e9291aca660dd8fc4b80712ce8306b123df
rishivyas1969 <rishivyas2424@gmail.com>
2021-09-07 00:54:51 +0530

    finished

--- main.py
+++ main.py
@@ -12,23 +12,28 @@ def icon_producer(ele):
     else:
         return "icons/red.png"
 
-map = folium.Map(location=[40.178, -99.903], zoom_start=4, tiles="CartoDB positron")
+map = folium.Map(location=[40.178, -99.903], zoom_start=3, tiles="CartoDB positron")
 
-fg = folium.FeatureGroup(name+"My Map")
+fgv = folium.FeatureGroup("Volcanoes")
 
 for i in range(len(data)):
     pop = str(data.iloc[i]["VOLCANX020"]) + "\n" + str(data.iloc[i]["NAME"])
 
     icon = folium.features.CustomIcon(icon_producer(data.iloc[i]["ELEV"]), icon_size=(28, 30))
 
-    fg.add_child(folium.Marker(
+    fgv.add_child(folium.Marker(
         location=[data.iloc[i]["LAT"], data.iloc[i]["LON"]],
         popup=pop,
         icon=icon))
 
-fg.add_child(folium.GeoJson(open("world.json", 'r', encoding="utf-8-sig").read(),
+fgp = folium.FeatureGroup("Population")
+
+fgp.add_child(folium.GeoJson(open("world.json", 'r', encoding="utf-8-sig").read(),
 style_function= lambda x: {'fillColor': 'green' if x["properties"]["POP2005"] < 10000000
 else 'orange' if 10000000 <= x["properties"]["POP2005"] < 20000000 else 'red'}))
 
-map.add_child(fg)
+map.add_child(fgv)
+map.add_child(fgp)
+
+map.add_child(folium.LayerControl())
 map.save("map.html")
